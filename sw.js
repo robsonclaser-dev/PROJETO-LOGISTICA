536c181ad3bbb2a0c060163369a61cbf37a88fb9
robsonclaser-dev <robsonclaser@gmail.com>
2026-06-15 12:48:45 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => new Response(''))));
